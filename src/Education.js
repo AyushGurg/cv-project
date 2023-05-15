d1a8f8e487853625b85f5f718611db04623ec52b
AyushGurg <120599569+AyushGurg@users.noreply.github.com>
2023-05-15 16:06:44 -0600

    finish layout #2

---
+++ src/Education.js
@@ -0,0 +1,13 @@
+function Education() {
+  return (
+    <form>
+      <input placeholder="University Name"></input>
+      <input placeholder="City"></input>
+      <input placeholder="Degree"></input>
+      <input placeholder="From"></input>
+      <input placeholder="To"></input>
+    </form>
+  );
+}
+
+export default Education;
---
+++ src/Experience.js
@@ -0,0 +1,13 @@
+function Experience() {
+  return (
+    <form>
+      <input placeholder="Position"></input>
+      <input placeholder="Company"></input>
+      <input placeholder="City"></input>
+      <input placeholder="From"></input>
+      <input placeholder="To"></input>
+    </form>
+  );
+}
+
+export default Experience;
