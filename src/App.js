02c6e7c567b6dc2d12c6072490f438d92d5acedf
AyushGurg <120599569+AyushGurg@users.noreply.github.com>
2023-05-15 16:05:37 -0600

    finish layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
+import Experience from "./Experience";
+import Education from "./Education";
 
 function App() {
   return (
-    <div >
-     raeact
-    </div>
+    <>
+      <div className="Header"> CV creator</div>
+      <div className="EditMode">
+        <div className="PersonalInformation">
+          <form>
+            <label>Personal Information</label>
+            <input placeholder="First Name"></input>
+            <input placeholder="Last Name"></input>
+            <input placeholder="Title"></input>
+            <input placeholder="Address"></input>
+            <input placeholder="Phone Number"></input>
+            <input placeholder="Email"></input>
+            <input placeholder="Description"></input>
+          </form>
+        </div>
+        <div className="Experience">
+          <label>Experience</label>
+          <Experience />
+        </div>
+        <div className="Education">
+          <label>Education</label>
+          <Education />
+        </div>
+      </div>
+    </>
   );
 }
 
