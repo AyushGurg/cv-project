72c117b4b795f8c960cf4bae4eb0a602bcb93648
AyushGurg <120599569+AyushGurg@users.noreply.github.com>
2023-05-18 18:54:59 -0600

    hooks started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Experience from "./Experience";
 import Education from "./Education";
+import { useState } from 'react';
 
 function App() {
+  
+    const [message, setMessage] = useState('');
+  
+    const handleChange = (event) => {
+      // 👇 Get input value from "event"
+      setMessage(event.target.value);
+    };
+
   return (
     <>
       <div className="Header"> CV creator</div>
@@ -9,13 +18,14 @@ function App() {
         <div className="PersonalInformation">
           <form>
             <label>Personal Information</label>
-            <input placeholder="First Name"></input>
+            <input placeholder="First Name" id="FirstName" onChange={handleChange}></input>
             <input placeholder="Last Name"></input>
             <input placeholder="Title"></input>
             <input placeholder="Address"></input>
             <input placeholder="Phone Number"></input>
             <input placeholder="Email"></input>
             <input placeholder="Description"></input>
+            <h2>{message}</h2>
           </form>
         </div>
         <div className="Experience">
