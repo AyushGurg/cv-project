f6279fc80456e4f4923e0e5dc1abeb54e74ae9c0
AyushGurg <120599569+AyushGurg@users.noreply.github.com>
2023-05-30 19:22:44 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import Experience from "./Experience";
-import Education from "./Education";
-import { useState } from 'react';
+// App.js
 
-function App() {
-  
-    const [message, setMessage] = useState('');
-  
-    const handleChange = (event) => {
-      setMessage(event.target.value);
-    };
+import React, { Component } from "react";
 
-  return (
-    <>
-      <div className="Header"> CV creator</div>
-      <div className="EditMode">
-        <div className="PersonalInformation">
-          <form>
-            <label>Personal Information</label>
-            <input placeholder="First Name" id="FirstName" onChange={handleChange}></input>
-            <input placeholder="Last Name"></input>
-            <input placeholder="Title"></input>
-            <input placeholder="Address"></input>
-            <input placeholder="Phone Number"></input>
-            <input placeholder="Email"></input>
-            <input placeholder="Description"></input>
-            <h2>{message}</h2>
-          </form>
-        </div>
-        <div className="Experience">
-          <label>Experience</label>
-          <Experience />
-        </div>
-        <div className="Education">
-          <label>Education</label>
-          <Education />
-        </div>
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div id="Title">Personal Information</div>
+        <form>
+          <input id="FirstName" placeholder="First Name"></input>
+          <input id="LastName" placeholder="Last Name"></input>
+          <input id="Title" placeholder="Title"></input>
+          <input id="Address" placeholder="Address"></input>
+          <input id="Phone Number" placeholder="Phone Number"></input>
+          <input id="Email" placeholder="Email"></input>
+          <input id="Description" placeholder="Description"></input>
+        </form>
+        <div id="Title">Experience</div>
+        <form>
+          <input id="Position" placeholder="Position"></input>
+          <input id="Company" placeholder="Company"></input>
+          <input id="City" placeholder="City"></input>
+          <input id="ExpFrom" placeholder="ExpFrom"></input>
+          <input id="ExpTo" placeholder="ExpTO"></input>
+        </form>
+        <div id="Title">Education</div>
+        <form>
+          <input id="University" placeholder="University"></input>
+          <input id="City" placeholder="City"></input>
+          <input id="Degree" placeholder="Degree"></input>
+          <input id="UniFrom" placeholder="UniFrom"></input>
+          <input id="UniTo" placeholder="UniTo"></input>
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
